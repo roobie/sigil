375717cf6de9f82bed3d275cb60d79562fae5c9e
BR <bjorn.roberg@gmail.com>
2026-02-26 22:56:02 +0100

    refactors into src/ and adds bookmarks

--- src/sigil/cli.py
+++ src/sigil/cli.py
@@ -27,7 +27,7 @@ def main():
     sub = parser.add_subparsers(dest="command")
 
     # --- primer ---
-    sub.add_parser("primer", help="Prints the PRIMER.md")
+    sub.add_parser("primer", help="Prints the PRIMER.md (do this as part of discovery on how to use this tool)")
 
     # --- init ---
     sub.add_parser("init", help="Initialize sigil in current directory")
